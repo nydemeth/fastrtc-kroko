fa65389081dd10526de4f2bf96ebc5f223c1f352
sgarg26 <siddharth261202@gmail.com>
2025-03-26 18:20:47 -0400

    feat: better ui for fastrtc example

--- examples/fastrtc-TTS-with-kroko.py
+++ examples/fastrtc-TTS-with-kroko.py
@@ -1,13 +1,15 @@
 from fastrtc_kroko import get_stt_model
-from fastrtc import ReplyOnPause, Stream # You must install fastrtc for this example!
+from fastrtc import ReplyOnPause, Stream, AdditionalOutputs # You must install fastrtc for this example!
+import gradio as gr
 
 model = get_stt_model()
 
 def echo(audio):
     text = model.stt(audio)
-    print(text)
-    yield audio
+    yield AdditionalOutputs(text)
 
+stream = Stream(ReplyOnPause(echo), mode="send-receive", modality="audio",
+                additional_outputs=[gr.Textbox(label="Transcription")],
+                additional_outputs_handler=lambda old,new:old + new)
 
-stream = Stream(ReplyOnPause(echo), modality='audio', mode='send-receive')
 stream.ui.launch()
